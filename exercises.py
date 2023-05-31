95a368d6b4dad6ace17b0c0e0a896449b0154e4b
Erasmo <jerasdiaz@yahoo.com>
2023-05-30 22:23:24 -0600

    second commit whit the loop while finished

--- exercises.py
+++ exercises.py
@@ -10,3 +10,18 @@ names.append(newName)
 for name in names:
     print(name)
 ###########################################################################
+print("###########################################################################")
+#A tuple with 7 items and print it using a while loop.
+
+# create the tupla
+namesT= ("jacob", "Montserrat", "Erasmo", "Carlos", "Sofía","Max","Victor")
+# create a counter and give it the value of "0"
+counter= 0
+#whit lean we know the length of the tuple
+repeat= len(namesT)
+#create the loop while and print the names
+#while counter is less than the length of the tuple, it will print what is inside
+while counter < repeat:
+    print (namesT[counter])
+    counter +=1
+###########################################################################
